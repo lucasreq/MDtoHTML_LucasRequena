bebce39f5183ce451244359c4d33b7a846b8e8f1
Lucas Requena <lucas.requena@ynov.com>
2019-02-06 22:32:09 +0100

    class

--- MDtoHTML.py
+++ MDtoHTML.py
@@ -15,12 +15,28 @@
 
 import re
 
-
 def extract_md(file_n,path):
     file = open(path+'\\'+ filename ,"r",encoding="utf-8")
     txt_lines = file.readlines()
     files.close()
     return txt_lines
+
+class Searcher:
+    def __init__(self, text_lines):
+        self.text_lines = text_lines
+        self.groups = None
+
+    def __contains__(self, regex):
+        match = re.search(regex, self.text_lines)
+        if match:
+            self.groups = match.groups()
+        else:
+            self.groups = None
+        return bool(match)
+
+    def __getitem__(self, index):
+        return self.groups[index]
+
 
 def html_files(file_n,path,txt_lines):
     file = open(path+'\\'+ filename[:-3] + ".html",encoding="utf-8")
@@ -37,43 +53,43 @@ def titles(txt_lines):
 
     result = []
     if '#' in txt:
-        result.append('<h3>' + txt[3:] + ' ' + '</h3>')
+        result.append('<h3>' + txt_lines[3:] + ' ' + '</h3>')
 
     elif '##' in txt:
-        result.append('<h2>' + txt[2:] + ' ' + '</h2>')
+        result.append('<h2>' + txt_lines[2:] + ' ' + '</h2>')
 
     elif '###' in txt:
-        result.append('<h1>' + txt[1:] + ' ' + '</h1>')
+        result.append('<h1>' + txt_lines[1:] + ' ' + '</h1>')
     return result
 
 def link(txt_lines):
     result = []
     if 'http://' in txt:
-        result.append('<a href=' + txt + '">' + txt + '</a>')
+        result.append('<a href=' + txt_lines + '">' + txt_lines + '</a>')
 
     elif 'https://' in txt:
-        result.append('<a href=' + txt + '">' + txt + '</a>')
+        result.append('<a href=' + txt_lines + '">' + txt_lines + '</a>')
     return result
 
 def important(txt_lines):
     result = []
     if '**' in txt:
-        result.append('<strong>' + txt + '</strong>')
+        result.append('<strong>' + txt_lines + '</strong>')
 
     elif '*' in txt:
-        result.append('<em>' + txt + '</em>')
+        result.append('<em>' + txt_lines + '</em>')
     return result
 
 def italic(txt_lines):
     result = []
     if '_' in txt:
-        result.append('<i>' + txt + '</i>')
+        result.append('<i>' + txt_lines + '</i>')
     return result
 
 def text(txt_lines):
     result = []
-    if txt in txt:
-        result.append('<p>' + txt + '</p>')
+    if txt_lines in txt_lines:
+        result.append('<p>' + txt_lines + '</p>')
     else:
         result.append('<br>')
     return result
